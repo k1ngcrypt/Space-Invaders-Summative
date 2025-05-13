6ec84301ec89cfea8f424e7fe45b6cbcf3837a98
chris-coding-conundrum <christopher.kenneth.walsh@gmail.com>
2025-05-13 14:29:23 -0400

    sa

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -13,7 +13,7 @@ export class MainMenu extends Scene
         this.add.image(window.innerWidth/2, window.innerHeight/2, 'logo');
 
         this.add.text(window.innerWidth/2, window.innerHeight/3*2, 'SPACE INVADERS', {
-            font-family: "Workbench", fontSize: 38, color: '#ffffff',
+            font-family: "Workbench, sans-serif", fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
