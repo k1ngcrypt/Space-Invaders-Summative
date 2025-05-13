8da50eee5333a50aee7e1ee2f6289be22e69c917
AdamB550 <abutt4@ocdsb.ca>
2025-05-13 14:48:24 -0400

    Main Js typewriter effect

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -1,22 +1,23 @@
 import { Scene } from 'phaser';
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
+export class MainMenu extends Scene {
+    constructor() {
         super('MainMenu');
     }
 
-    create ()
-    {
+    create() {
 
-        this.add.image(window.innerWidth/2, window.innerHeight/2, 'logo');
+        this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'logo');
 
-        this.add.text(window.innerWidth/2, window.innerHeight/3*2, 'SPACE INVADERS', {
-            fontFamily: 'Workbench, sans-serif', fontSize: 38, color: '#ffffff',
+        this.add.text(window.innerWidth / 2, window.innerHeight / 3 * 2, 'SPACE INVADERS',
+            font - family: "Workbench", fontSize: 38, color: '#ffffff', {
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
+
+        setInterval (() => {current += full[i++];
+            Display.setText(current); 
+        }, 100); 
 
         this.input.once('pointerdown', () => {
 
