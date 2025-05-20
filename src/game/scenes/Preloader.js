5236c7d10805a1677e2f449770429183e279fbff
chris-coding-conundrum <christopher.kenneth.walsh@gmail.com>
2025-05-20 14:20:01 -0400

    Centering progress Bar

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -10,7 +10,7 @@ export class Preloader extends Scene
     init ()
     {
         //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        this.add.rectangle((window.innerWidth/2), (window.innerHeight/2), 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
         const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
