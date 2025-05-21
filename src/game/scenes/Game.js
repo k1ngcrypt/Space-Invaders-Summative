941daaf0588b53b7d17a4c74f46c65dd7edb5521
k1ngcrypt <118375520+k1ngcrypt@users.noreply.github.com>
2025-05-21 10:19:08 -0400

    enemymap

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -14,20 +14,16 @@ export class Game extends Scene
         const rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         const leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         const spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
+        const enemies = ['A', 'B','B','C', 'C'];
         let enemymap = [];
         for (let i=0; i<12; i++) {
             enemymap[i] = [];
             for (let j=0; j<5; j++) {
-                switch(i){
-                    case 0:
-                        
-                }
+                enemymap[i][j] = this.add.sprite(100 + i * 50, 100 + j * 50, enemies[j] + '-Animation').setOrigin(0.5, 0.5);
             }
         }
         
 
-        
         this.input.once('pointerdown', () => {
 
             this.scene.start('GameOver');
