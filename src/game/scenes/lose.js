c14836461b91ab847e0f36e53841c3d14ee43c4d
chris-coding-conundrum <christopher.kenneth.walsh@gmail.com>
2025-05-30 14:52:52 -0400

    Changing win and Lose screens

--- src/game/scenes/lose.js
+++ src/game/scenes/lose.js
@@ -14,7 +14,7 @@ init(){
         this.cameras.main.setBackgroundColor(0xff0000);
 
         this.add.text(512, 384, ('Score: '+String(score)), {
-            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
+            fontFamily: 'Workbench', fontSize: 64, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
--- src/game/scenes/win.js
+++ src/game/scenes/win.js
@@ -13,8 +13,8 @@ init(){
     {
         this.cameras.main.setBackgroundColor(0xff0000);
 
-        this.add.text(512, 384, ('Score: '+String(score)), {
-            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
+        this.add.text(512, 384, ('You Win!'), {
+            fontFamily: 'Workbench', fontSize: 64, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
