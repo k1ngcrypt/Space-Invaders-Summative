a32f0017ec285a03b9f0aaac749e527edd221c2f
k1ngcrypt <118375520+k1ngcrypt@users.noreply.github.com>
2025-06-08 13:48:42 -0400

    prepping for build

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -17,7 +17,7 @@ const phasermsg = () => {
 }   
 
 export default defineConfig({
-    base: '/Space-Invaders-Summative',
+    base: '/',
     logLevel: 'warn',
     build: {
         rollupOptions: {
